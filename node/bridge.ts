c65dfd59b94e5de5725575f6c69feeba5d04af63
duasfh <duasfh@gmail.com>
2025-12-29 21:18:55 +0100

    - _ debug_mem

--- node/bridge.ts
+++ node/bridge.ts
@@ -71,4 +71,45 @@ const debug_full = () => {
   console.log(rs.get_mem())
 }
 
-debug_full()
+const debug_mem = () => {
+  console.log(rs.get_size())
+  console.log(rs.get_mem())
+
+  rs.set_int('a', 23)
+
+  console.log(rs.get_size())
+  console.log(rs.get_mem())
+
+  rs.set_int('a', 24)
+  rs.set_int('b', 25)
+  rs.set_int('c', 26)
+
+  console.log(rs.get_size())
+  console.log(rs.get_mem())
+
+  rs.del_int('a')
+
+  console.log(rs.get_size())
+  console.log(rs.get_mem())
+
+  rs.clear()
+
+  console.log(rs.get_size())
+  console.log(rs.get_mem())
+
+  /**
+    ->
+    0
+    0
+    1
+    16
+    1
+    64
+    0
+    48
+    0
+    0
+  */
+}
+
+debug_mem()
