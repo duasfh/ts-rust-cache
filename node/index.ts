4025b52e10cbcf0d93d3f979eebe4c1da03cb4d4
duasfh <duasfh@gmail.com>
2026-01-04 17:26:31 +0100

    - bump version `0.1.1` -> `0.2.0` - update package index: bridge -> index (which re-exports all from bridge + adds type wrapper)

--- node/index.ts
+++ node/index.ts
@@ -17,15 +17,3 @@ type TypedCache<CD extends CacheDesc> = {
 export const init = <T extends CacheDesc> () => {
   return cache as TypedCache<T>
 }
-
-// todo ad as example:
-// type Cache = {
-//   staticKey: number
-// } & {
-//   [K in `email-verification-${string}`]: string
-// }
-// const a = init<Cache>()
-// a.set('staticKey', 2, 2)
-// a.set('email-verification-string', '2', 2)
-// const b = a.get('staticKey')
-// const b2 = a.get('email-verification-string')
