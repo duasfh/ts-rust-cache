4c4a0cd72236cc77741ed69e5c3a94ed9afbb107
Pavel Shamparov <duasfh@gmail.com>
2026-01-04 13:10:00 +0100

    test: add unit tests (#12)
      - add test coverage; setup jest
      - expose _initIntervalCleanup
    ser (sorry)
      - fix isSerializable
      - readme: specify case behaviour for storing objects with nested Dates
    
    closes #9
    related to #11

--- node/utils.ts
+++ node/utils.ts
@@ -1,6 +1,8 @@
 import libms, { type StringValue as MsStringValue }  from 'ms'
 
 export const isSerializable = (obj: object) => {
+  if (isClassInstance(obj)) return false
+
   try {
     structuredClone(obj)
     return true
@@ -8,6 +10,12 @@ export const isSerializable = (obj: object) => {
     return false
   }
 }
+
+const isClassInstance = (obj: unknown) => {
+  if (obj === null || typeof obj !== 'object') return false
+  return Object.getPrototypeOf(obj) !== Object.prototype
+}
+
 
 /** @note strict version of 'ms', since it's not shipped in 2.1.3 */
 export const ms = (string: MsStringValue) => {
